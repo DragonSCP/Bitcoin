2e2d6dc430d9c8fbfe4e12153a43d1a164237f61
DragonSCP <135724825+DragonSCP@users.noreply.github.com>
2024-04-28 14:26:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-from miners.bitcoin import bitcoin
+import subprocess
+import importlib
+
+# Dicionário para mapear opções de criptomoedas e seus módulos correspondentes
+cryptocurrencies = {
+    "1": ("Bitcoin", "miners.bitcoin.bitcoin")
+}
+
+def load_module(crypto_module):
+    """Função para carregar dinamicamente módulos de mineração."""
+    module = importlib.import_module(crypto_module)
+    return module
 
 def main():
     print("Bem-vindo ao Script de Mineração de Criptomoedas!")
     print("Selecione a criptomoeda para mineração:")
-    print("1. Bitcoin")
-    # Futuras opções podem ser adicionadas aqui
+    for key, value in cryptocurrencies.items():
+        print(f"{key}. {value[0]}")
     choice = input("Digite o número da opção escolhida: ")
 
-    if choice == '1':
-        print("Você selecionou Bitcoin.")
-        wallet_address = input("Por favor, insira o endereço da sua carteira de Bitcoin: ")
-        pool_host = "pool.supportxmr.com:3333"  # Especifique o pool host
-        print("Iniciando a mineração de Bitcoin no pool " + pool_host)
-        bitcoin.mine(wallet_address, pool_host)
+    if choice in cryptocurrencies:
+        crypto_name, crypto_module = cryptocurrencies[choice]
+        print(f"Você selecionou {crypto_name}.")
+        wallet_address = input("Por favor, insira o endereço da sua carteira: ")
+        pool_host = "pool.supportxmr.com:3333"  # Especifique o pool host aqui ou peça ao usuário
+        print(f"Iniciando a mineração de {crypto_name} no pool {pool_host}...")
+        mining_module = load_module(crypto_module)
+        mining_module.mine(wallet_address, pool_host)
     else:
         print("Opção inválida!")
 
